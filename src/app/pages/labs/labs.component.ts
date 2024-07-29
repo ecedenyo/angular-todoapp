f6b20b6fd2a71e74f124a840269dd51e08658a70
Emerson Cedeno <emerson.cedeno@logicstudio.net>
2024-07-28 23:58:14 -0500

    :sparkles: general angular template syntax

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css',
+})
+export class LabsComponent {
+  my_name = 'Emerson';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
+    'Crear servicio',
+  ];
+}
